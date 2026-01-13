d811f1543dde9b10e3497faa50ae87f4579b02b7
Mostafa Jarwan <41651412+Mostafa563@users.noreply.github.com>
2026-01-13 13:10:40 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// item = document.querySelector(".unit");
+// cal = document.getElementById("calender");
+// item.addEventListener("mouseenter", func, false);
+// function func() {
+//   cal.setAttribute("style", "filter: grayscale(60%) drop-shadow(0 0 15px rgb(85, 85, 85));animation: bounce 0.6s ease-in-out 3;");
+// }
+
+
